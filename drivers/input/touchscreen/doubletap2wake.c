e2df7be8ba69ec41328751acb7871f5418556f23
Ryan Andri <ryanandri@linuxmail.org>
2017-12-29 08:21:50 +0700

    input: dt2w/s2w: reduce waking up time.
    Signed-off-by: Ryan Andri <ryanandri@linuxmail.org>

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -56,7 +56,7 @@ MODULE_LICENSE("GPLv2");
 #define DT2W_DEBUG		0
 #define DT2W_DEFAULT		0
 
-#define DT2W_PWRKEY_DUR		60
+#define DT2W_PWRKEY_DUR		30
 #define DT2W_FEATHER		200
 #define DT2W_TIME		700
 
